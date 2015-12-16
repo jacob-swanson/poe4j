bbf6ebfeb6dc5ef70e5e30806e3a5d38020645f7
Jacob Swanson <jacob@swandiggy.com>
2015-12-16 07:30:17 -0600

    Make rows load lazily

--- poe4j/src/main/java/com/swandiggy/poe4j/data/readers/value/RowReader.java
+++ poe4j/src/main/java/com/swandiggy/poe4j/data/readers/value/RowReader.java
@@ -6,6 +6,8 @@ import com.swandiggy.poe4j.data.DatFileReader;
 import com.swandiggy.poe4j.data.DatFileReaderFactory;
 import com.swandiggy.poe4j.data.rows.AbstractRow;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.LazyLoader;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -33,12 +35,18 @@ public class RowReader extends BaseValueReader<AbstractRow> {
             return null;
         }
 
-        // Get the referenced .dat file
-        try (DatFileReader datFileReader = datFileReaderFactory.create(Paths.get(reader.getFile().getParent(), reader.getEntityClasses().inverse().get(clazz) + ".dat").toFile())) {
-            return datFileReader.read(index);
-        } catch (IOException e) {
-            throw new Poe4jException(e);
-        }
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(clazz);
+        enhancer.setCallback((LazyLoader) () -> {
+            // Get the referenced .dat file
+            try (DatFileReader datFileReader = datFileReaderFactory.create(Paths.get(reader.getFile().getParent(), reader.getEntityClasses().inverse().get(clazz) + ".dat").toFile())) {
+                return datFileReader.read(index);
+            } catch (IOException e) {
+                throw new Poe4jException(e);
+            }
+        });
+
+        return (AbstractRow) enhancer.create();
     }
 
     @Override
