591fcbe465481fa1064fb6506a58311ebc0181f0
Jacob Swanson <jacob@swandiggy.com>
2016-02-02 19:36:14 -0600

    Fix webpack formatting

--- poe4j-web/webpack.config.js
+++ poe4j-web/webpack.config.js
@@ -9,14 +9,10 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" },
-            { test: /\.json/, loader: "json" },
-            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
-            {
-                test: /\.html$/,
-                loader: 'ngtemplate!html',
-                exclude: /index.html/
-            }
+            {test: /\.css$/, loader: "style!css"},
+            {test: /\.json/, loader: "json"},
+            {test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000'},
+            {test: /\.html$/, loader: 'ngtemplate!html', exclude: /index.html/}
         ]
     },
     plugins: [new HtmlWebpackPlugin({
