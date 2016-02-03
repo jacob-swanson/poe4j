6ff530f7b5862b5e88cfa72696addca2dcc4bd64
Jacob Swanson <jacob@swandiggy.com>
2016-02-02 20:48:22 -0600

    Add extracting data without extracting Content.ggpk

--- poe4j/src/main/java/com/swandiggy/poe4j/data/DatFileLookup.java
+++ poe4j/src/main/java/com/swandiggy/poe4j/data/DatFileLookup.java
@@ -2,9 +2,13 @@ package com.swandiggy.poe4j.data;
 
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
+import com.swandiggy.poe4j.Poe4jException;
 import com.swandiggy.poe4j.config.Poe4jProperties;
 import com.swandiggy.poe4j.data.annotations.DatFile;
 import com.swandiggy.poe4j.data.rows.BaseRow;
+import com.swandiggy.poe4j.ggpkg.Ggpk;
+import com.swandiggy.poe4j.ggpkg.GgpkExtractor;
+import com.swandiggy.poe4j.ggpkg.GgpkFactory;
 import lombok.Setter;
 import org.reflections.Reflections;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +17,7 @@ import org.springframework.util.StringUtils;
 
 import java.io.File;
 import java.nio.file.Paths;
+import java.text.MessageFormat;
 
 /**
  * Find a .dat file that goes with a class or find a class that goes with a .dat file.
@@ -25,11 +30,19 @@ public class DatFileLookup {
     @Setter
     private Poe4jProperties properties;
 
+    @Setter
+    private GgpkFactory ggpkFactory;
+
+    @Setter
+    private GgpkExtractor ggpkExtractor;
+
     public DatFileLookup() {
     }
 
-    public DatFileLookup(Poe4jProperties properties) {
+    public DatFileLookup(Poe4jProperties properties, GgpkFactory ggpkFactory, GgpkExtractor ggpkExtractor) {
         this.properties = properties;
+        this.ggpkFactory = ggpkFactory;
+        this.ggpkExtractor = ggpkExtractor;
     }
 
     /**
@@ -53,7 +66,19 @@ public class DatFileLookup {
     }
 
     public <T extends BaseRow> File getFileForType(Class<T> clazz) {
-        return Paths.get(properties.getGgpkDirectory(), "Data", entityClasses.inverse().get(clazz) + ".dat").toFile();
+        File file = new File(properties.getGgpkDirectory());
+        if (!file.exists()) {
+            throw new Poe4jException(MessageFormat.format("GGPK {0} not found", file));
+        }
+
+        if (file.isDirectory()) {
+            return Paths.get(properties.getGgpkDirectory(), "Data", entityClasses.inverse().get(clazz) + ".dat").toFile();
+        } else if (file.isFile()) {
+            Ggpk ggpk = ggpkFactory.load(new File("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Path of Exile\\Content.ggpk"));
+            return ggpkExtractor.getFileRecord(ggpk, Paths.get("Data", entityClasses.inverse().get(clazz) + ".dat").toString());
+        } else {
+            throw new Poe4jException(MessageFormat.format("GGPK {0} not a file or directory", file));
+        }
     }
 
     public <T extends BaseRow> Class<T> getTypeForFile(File file) {
--- poe4j/src/main/java/com/swandiggy/poe4j/ggpkg/Ggpk.java
+++ poe4j/src/main/java/com/swandiggy/poe4j/ggpkg/Ggpk.java
@@ -1,13 +1,26 @@
 package com.swandiggy.poe4j.ggpkg;
 
+import com.swandiggy.poe4j.Poe4jException;
+import com.swandiggy.poe4j.ggpkg.record.DirectoryRecord;
+import com.swandiggy.poe4j.ggpkg.record.FileRecord;
 import com.swandiggy.poe4j.ggpkg.record.Record;
 import com.swandiggy.poe4j.util.collection.Node;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.util.Assert;
 
-import java.io.File;
+import java.io.*;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.text.MessageFormat;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * @author Jacob Swanson
