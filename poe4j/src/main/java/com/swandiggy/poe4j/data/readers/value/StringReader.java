e2b320f83dc8aa78f570434f9b8729e5a2c1703a
Jacob Swanson <jacob@swandiggy.com>
2016-02-12 19:48:55 -0600

    Add check if string is at EOF

--- poe4j/src/main/java/com/swandiggy/poe4j/data/readers/value/StringReader.java
+++ poe4j/src/main/java/com/swandiggy/poe4j/data/readers/value/StringReader.java
@@ -1,7 +1,7 @@
 package com.swandiggy.poe4j.data.readers.value;
 
 import com.swandiggy.poe4j.data.DatFileReader;
-import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Read a string value. Reads an int pointer into the variable width portion of the file. Strings are null terminated
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
  * @author Jacob Swanson
  * @since 12/15/2015
  */
+@Slf4j
 public class StringReader extends BaseValueReader<String> {
 
     public StringReader() {
@@ -25,6 +26,11 @@ public class StringReader extends BaseValueReader<String> {
         int ref = reader.getBr().readInt();
 
         long oldPos = reader.getBr().getPosition();
+        long stringPos = reader.getDataOffset() + ref;
+        if (stringPos >= reader.getFile().length()) {
+            log.warn("String was at end of file eof: '{}', string ref: '{}'", reader.getFile().length(), stringPos);
+            return null;
+        }
         reader.getBr().setPosition(reader.getDataOffset() + ref);
         String s = reader.getBr().readString("UTF-16LE");
         reader.getBr().setPosition(oldPos);
